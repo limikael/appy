e885e2e8661318eeb3e8ae4510d57153416ddde3
Mikael Lindqvist <li.mikael@gmail.com>
2023-03-21 12:49:03 +0800

    noparam?

--- packages/appy/src/tests.rs
+++ packages/appy/src/tests.rs
@@ -1,9 +1,10 @@
 use crate::{*};
 use std::rc::Rc;
+use std::any::Any;
 
 // dyn Fn()
 
-#[derive(Clone)]
+/*#[derive(Clone)]
 struct Cb {
     f: Rc<dyn Fn()>,
 }
@@ -22,22 +23,26 @@ impl std::ops::Deref for Cb {
     fn deref(&self) -> &Self::Target {
         &self.f
     }
-}
+}*/
+
+pub struct NoParam {}
+pub const NO_PARAM:NoParam=NoParam{};
 
 #[derive(Clone)]
-struct CbP<P> {
+struct CbX<P> {
     f: Rc<dyn Fn(P)>,
 }
 
-impl<P, T: Fn(P) + 'static> From <T> for CbP<P> {
-	fn from(f: T)->CbP<P> {
+impl<P, T: Fn(P) + 'static> From <T> for CbX<P> {
+	fn from(f: T)->CbX<P> {
 	    Self {
 	        f: Rc::new(f) //||println!("hello")),
 	    }
 	}
 }
 
-impl<P> std::ops::Deref for CbP<P> {
+impl<P> std::ops::Deref for CbX<P>
+		where P:Any {
     type Target = Rc<dyn Fn(P)>;
 
     fn deref(&self) -> &Self::Target {
@@ -45,11 +50,20 @@ impl<P> std::ops::Deref for CbP<P> {
     }
 }
 
+/*impl<P> std::ops::Deref for CbX<P>
+		where P:Any {
+    type Target = Rc<dyn Fn(P)>;
+
+    fn deref(&self) -> &Self::Target {
+        &self.f
+    }
+}*/
+
 #[test]
 fn test() {
-	let cb=Cb::from(||println!("hello..."));
-	cb();
+	/*let cb=Cb::from(||println!("hello..."));
+	cb();*/
 
-	let cb=CbP::from(|i:(i32)|println!("hello: {}",i));
-	cb((123));
+	let cb:CbX<NoParam>=CbX::from(|_|println!("hello"));
+	cb(NO_PARAM);
 }
