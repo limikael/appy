f9e7e62965562d74fe6f8e0885bbf45f1594b7e2
Mikael Lindqvist <li.mikael@gmail.com>
2023-04-07 16:47:10 +0800

    hello

--- examples/hello/src/main.rs
+++ examples/hello/src/main.rs
@@ -4,9 +4,9 @@ use appy::{*};
 pub fn app()->Elements {
 	apx!{
 		<bg col=0x102030/>
-		<text text="Hello World".to_string() align=Align::Center size=Pc(10.0)/>
-		<blk top=Pc(0.0) left=Pc(0.0) width=Pc(25.0) height=Pc(25.0)>
-			<bg col=0x00ffff/>
+		<blk top=Pc(40.0) width=Pc(100.0) height=Pc(20.0)>
+			<bg col=0x008080/>
 		</blk>
+		<text text="Hello World".to_string() align=Align::Center size=Pc(10.0)/>
 	}
 }
